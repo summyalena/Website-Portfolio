74229047c7349f52500538c9e9b6e1b3ed73c5df
Lena <summyalena@gmail.com>
2022-07-18 14:18:33 +0100

    We created a mobile-menu for my portfolio using javascript and configured our linters correctly

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-const mobileNav = document.querySelector(".mobile-nav")
-const hamburgerMenu = document.querySelector(".hamburger")
-const hamburgerClose = document.querySelector(".hamburger-close")
-const mobileNavLink = document.querySelectorAll(".mobile-nav-link")
+const mobileNav = document.querySelector('.mobile-nav');
+const hamburgerMenu = document.querySelector('.hamburger');
+const hamburgerClose = document.querySelector('.hamburger-close');
+const mobileNavLink = document.querySelectorAll('.mobile-nav-link');
 
-hamburgerMenu.addEventListener("click", () => {
-  mobileNav.classList.toggle("is-active")
-  hamburgerClose.classList.toggle("is-active")
-})
-
-mobileNavLink.forEach(link => {
-  link.addEventListener("click", () => {
-    mobileNav.classList.toggle("is-active")
-    hamburgerClose.classList.toggle("is-active")
-  })
+hamburgerMenu.addEventListener('click', () => {
+  mobileNav.classList.toggle('is-active');
+  hamburgerClose.classList.toggle('is-active');
 });
 
+mobileNavLink.forEach((link) => {
+  link.addEventListener('click', () => {
+    mobileNav.classList.toggle('is-active');
+    hamburgerClose.classList.toggle('is-active');
+  });
+});
