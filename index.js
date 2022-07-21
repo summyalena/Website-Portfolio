82a999809493b1dab376ee594bcc1e59ba828389
Inieke Sendy <ini.e.sendy@gmail.com>
2022-07-21 03:45:16 +0100

    Update: Card Section and Object creation with JS

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let cardWorks = {
   card1: {
     id:1,
      title:'Professional Art Printing Data',
-     description:'A daily selection of privately personalized reads no accounts or sign-ups required. has been the industry standard',
+     description:"A daily selection of privately personalized reads no accounts or sign-ups required. has been the industry standard Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
      featuredImage:'./images/popimage.png',
      technologies:[
       'Html',
@@ -15,7 +15,7 @@ let cardWorks = {
  card2: {
     id:2,
      title:'Professional Art Printing Data',
-     description:'A daily selection of privately personalized reads no accounts or sign-ups required. has been the industry standard',
+     description:"A daily selection of privately personalized reads no accounts or sign-ups required. has been the industry standard Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
      featuredImage:'./images/popimage.png',
      technologies:{
       span1: 'Html',
@@ -28,7 +28,7 @@ let cardWorks = {
  card3: {
     id:3,
      title:'Professional Art Printing Data',
-     description:'A daily selection of privately personalized reads no accounts or sign-ups required. has been the industry standard',
+     description:"A daily selection of privately personalized reads no accounts or sign-ups required. has been the industry standard Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
      featuredImage:'./images/popimage.png',
      technologies:[
       'Html',
@@ -41,7 +41,7 @@ let cardWorks = {
   card4: {
     id:4,
      title:'Professional Art Printing Data',
-     description:'A daily selection of privately personalized reads no accounts or sign-ups required. has been the industry standard',
+     description:"A daily selection of privately personalized reads no accounts or sign-ups required. has been the industry standard Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
      featuredImage:'./images/popimage.png',
      technologies:[
       'Html',
@@ -54,7 +54,7 @@ let cardWorks = {
  card5:  {
     id:5,
      title:'Professional Art Printing Data',
-     description:'A daily selection of privately personalized reads no accounts or sign-ups required. has been the industry standard',
+     description:"A daily selection of privately personalized reads no accounts or sign-ups required. has been the industry standard Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
      featuredImage:'./images/popimage.png',
      technologies:[
       'Html',
@@ -67,7 +67,7 @@ let cardWorks = {
   card6: {
     id:6,
      title:'Professional Art Printing Data',
-     description:'A daily selection of privately personalized reads no accounts or sign-ups required. has been the industry standard',
+     description:"A daily selection of privately personalized reads no accounts or sign-ups required. has been the industry standard Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
      featuredImage:'./images/popimage.png',
      technologies:[
       'Html',
@@ -82,65 +82,77 @@ let cardWorks = {
 const cardSection = document.querySelector('.cardworks')
 
 for(const cards in cardWorks){
-  if(Object.hasOwnProperty.call(cardWorks,cards)){
+  if(Object.hasOwnProperty.call(cardWorks, cards)){
    const card =  cardWorks[cards]
- console.log(card)
   cardSection.innerHTML += `
   <div class="cardimages">
-  <div class=" cardwork cardwork1">
-  <div class="cardcon">
-    <h3>${card.title}</h3>
-    <p>
-     ${card.description}
-    </p>
-    <div class="spancon">
-      <span class="span">HTML</span>
-      <span class="span">CSS</span>
-      <span class="span">JAVASCRIPT</span>
+    <div class="cardwork-container">
+      <div class=" cardwork cardwork1">
+        <div class="cardcon">
+          <h3>${card.title}</h3>
+          <p>
+          ${card.description.length > 90 ? card.description.substring(0,90) : card.description}
+          </p>
+          <div class="spancon">
+            <span class="span">HTML</span>
+            <span class="span">CSS</span>
+            <span class="span">JAVASCRIPT</span>
+          </div>
+        </div>
+        <div class="button">
+          <a href="" id= "${card.id}" class="see" >See Project</a>
+        </div>
+      </div>
     </div>
-  </div>
-  <div class="button">
-    <a href="" id=${card.id} class="see" type="button">See Project</>
-  </div>
-  </div>
-  </div>
-  </div>
-</div>`
+  </div>`
+}
 }
 
-}
+const projectPopup = document.querySelector('.project-popup')
+console.log(projectPopup)
+const popButton = document.querySelectorAll('.see')
+console.log(popButton)
 
-const popButton = document.querySelectorAll('.button')
-
-popButton.forEach(popup=>{
+popButton.forEach(popup=> {
   popup.addEventListener('click', (e)=>{
     e.preventDefault()
-    console.log(popup.id)
     for(const cards in cardWorks){
       if(Object.hasOwnProperty.call(cardWorks,cards)){
         const card = cardWorks[cards]
-        console.log(card)
-      cardSection.innerHTML += `
-      <div class="cardimages">
-      <div class=" cardwork cardwork1">
-      <div class="cardcon">
-        <h3>${card.title}</h3>
-        <p>
-         ${card.description}
-        </p>
-        <div class="spancon">
-          <span class="span">HTML</span>
-          <span class="span">CSS</span>
-          <span class="span">JAVASCRIPT</span>
-        </div>
-      </div>
-      <div class="button">
-        <a href="" class="see" type="button">See Project</>
-      </div>
-      </div>
-      </div>
-      </div>
-    </div>`
+          if (card.id == popup.id) {
+            projectPopup.innerHTML = `
+              <div class="popup-container">
+                <div class="popup-cardwork">
+                  <div class = "title-popup">
+                    <h1>${card.title}</h1>
+                    <div class= "popup-close-btn">&times;</div>
+                  </div>
+                  <div class="popup-spancon">
+                      <span class="popup-span">HTML</span>
+                      <span class="popup-span">CSS</span>
+                      <span class="popup-span">JAVASCRIPT</span>
+                  </div>
+                  <div class="popup-image">
+                  <img class = "image-pop" src="${card.featuredImage}" alt="images">
+                  </div>
+
+                  
+                    <p class="popup-description">
+                      ${card.description}
+                    </p>
+                  
+                  <div class="spancon">
+                    <span class="span">HTML</span>
+                    <span class="span">CSS</span>
+                    <span class="span">JAVASCRIPT</span>
+                  </div>
+                  <div class="button">
+                    <a href="" class="see" type="button">See Project</a>
+                    <a href="" class="see" type="button">See Project</a>
+                  </div>
+                </div>
+              </div>`
+          }
     }
   }
     
